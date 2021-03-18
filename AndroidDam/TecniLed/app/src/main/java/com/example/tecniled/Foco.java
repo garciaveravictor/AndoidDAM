3600be3809dbfa3dc93252e4b92cd3d3ede6ebd9
garciaveravictor <71645624+garciaveravictor@users.noreply.github.com>
2021-03-18 13:21:20 +0100

    TecniLed

---
+++ AndroidDam/TecniLed/app/src/main/java/com/example/tecniled/Foco.java
@@ -0,0 +1,18 @@
+package com.example.tecniled;
+
+public class Foco {
+    private String nombre;
+    private String marca;
+    private int direccion;
+    private int canales;
+    private int dmx;
+
+    public Foco(String nombre, String marca, int direccion, int canales, int dmx) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.direccion = direccion;
+        this.canales = canales;
+        this.dmx = dmx;
+    }
+
+}
